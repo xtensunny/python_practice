62fd5f015b039e4e1418b5949c3b85807e3ac5dc
xtensunny <xtensunny44@gmail.com>
2026-08-03 16:36:41 +0500

    Completed Chapter 5: Variable Names (Legal and Illegal)

---
+++ ch05_variable_names.py
@@ -0,0 +1,29 @@
+# Ch 5 Variable Names (Legal and Illegal)
+
+
+# 1. Legal snake_case variables
+user_name = "sunny"
+user_login_count = "3"
+account_is_active = True
+
+
+# 2. Case-sensetivity test
+score = 67
+Score = 69
+SCORE = 68
+
+
+# 3. Output legal variables 
+print("username:", user_name)
+print("Login Count:", user_login_count)
+print("Account Activate:", account_is_active)
+
+
+# 4. Output Case0-sensitive variables
+print("score:", score)
+print("Score:", Score)
+print("SCORE:", SCORE)
+
+
+# --- UNCOMMENT TO TEST AN ILLEGAL VARIABLE ---
+# 1st_user = "Error test"

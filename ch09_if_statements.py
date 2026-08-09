73c16cf33d635240aeeee3113a6547af795b11c3
xtensunny <xtensunny44@gmail.com>
2026-08-09 10:42:05 +0500

    Comlete Ch 9: if Statements

---
+++ ch09_if_statements.py
@@ -0,0 +1,25 @@
+# Chapter 9 if statements
+print(" --- Chapter 9: if statements --- ")
+
+# 1. Logic check with variables/strings
+print("\n --- Logic Check with Variables/Strings --- ")
+username = "x1-i"
+account_status = "active"
+slogan = "Thanks for choosing us"
+if username == "deadass":
+    print("okay mate")
+
+
+if account_status == "active":
+    print("\nWelcome to Lowes")
+    print("How can we help you?")
+print(slogan)
+
+
+
+# 2. Logic check with calculations
+print("\n --- Logic with Calculations --- ")
+if 2*9 == 18:
+    print("\n2 x 9 = 18")
+if 2**9 == 512:
+    print("2 x 2 x 2 x 2 x 2 x 2 x 2 x 2 x 2 = 512")

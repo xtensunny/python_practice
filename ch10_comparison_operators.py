a5091086517868c0c8e4c7fff469071adff2f13c
xtensunny <xtensunny44@gmail.com>
2026-08-09 11:36:32 +0500

    Complete Ch 10: Comparison Operators

---
+++ ch10_comparison_operators.py
@@ -0,0 +1,31 @@
+# Chapter 10 Comparison Operators
+print(" --- Chapter 10: Comparison Operators --- ")
+
+# 1. Testing all comparison operators
+print("\n ---Testing all comparison operators--- ")
+
+one = 1
+two = 2
+three = 3
+four = 4
+five = 5
+six = 6
+if one == 1:
+    print("\nOne = 1")
+if two != 2:
+    print("two != 2")
+if three < four:
+    print("3 < 4")
+if four > five:
+    print("4 > 5")
+if five <= 5*1:
+    print("5 <= 5")
+if six >= 5:
+    print("6 >= 5")
+
+
+# 2. Case sensitivity check
+print("\n ---Case-sensitivity check--- ")
+
+if "Jin" != "jin":
+    print("\nJin isn't same as jin")

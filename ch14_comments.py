119c75fc2aeca6797a5791a589279fd30dd90427
xtensunny <xtensunny44@gmail.com>
2026-08-11 14:58:04 +0500

    Complete Ch 14: Comments

---
+++ ch14_comments.py
@@ -0,0 +1,12 @@
+# Chapter 14 Comments
+print(" --- Chapter 14: Comments --- ")
+
+# This is a comment and it is ignored by python
+print("Single line Comment")
+
+print("Inline Comment") # im an inline comment
+'''
+single quotes thrice for multi line comments
+print("multiple line comment")
+''' 
+print("Multi-line Comment")
